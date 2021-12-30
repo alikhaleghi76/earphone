cf12413f80fb4e2af898e6f0f0109ff97ae383f6
Ali Khaleghi <a.khaleghi@asredanesh.com>
2021-12-30 08:58:56 +0330

    Empty project

---
+++ app/src/main/java/ali/khaleghi/earphone/MainActivity.kt
@@ -0,0 +1,11 @@
+  package ali.khaleghi.earphone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
